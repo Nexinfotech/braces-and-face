cae0035d05c11f20f380b33db3e3be38afbcb02c
hritik222 <rajritik5132@gmail.com>
2023-07-08 14:32:01 +0530

    push 2 about section

--- index.php
+++ index.php
@@ -52,44 +52,39 @@
         <!-- specification section start -->
         <section>
             <div class="specs_container">
-                <div class="container py-lg-4 py-2">
-                    <div class="row align-items-center justify-content-between">
-
-                        <div class="col-md-2 ">
-                            <div class="specs_box">
-                                <img src="assets/img/5start.png" alt="" class="img-fluid">
-                                <h4>Star Ratings</h4>
+                <div class="container py-2">
+                    <div class="achieve">
+                        <div class="d-lg-block d-md-none d-sm-none d-none">
+                            <div class="row align-items-center justify-content-center">
+                                <div class="col border pt-lg-0 pt-md-0 pt-4">
+                                    <div class="abox">
+                                        <img src="assets/img/5start.png" class="img-fluid">
+                                        <p>Star Rating</p>
+                                        <span></span>
+                                    </div>
+                                </div>
+                                <div class="col border pt-lg-0 pt-md-0 pt-4">
+                                    <div class="abox">
+                                        <div class="vector">
+                                            <img src="assets/img/100per.png" class="img-fluid">
+                                        </div>
+                                        <p>Satisfaction Guarantee</p>
+                                    </div>
+                                </div>
+                                <div class="col border pt-lg-0 pt-4">
+                                    <div class="abox three-four">
+                                        <img src="assets/img/relief.png" class="img-fluid">
+                                        <p>Insant Results</p>
+                                    </div>
+                                </div>
+                                <div class="col  pt-lg-0 pt-4">
+                                    <div class="abox three-four">
+                                        <img src="assets/img/scinetiestpro.svg" class="img-fluid">
+                                        <p>Scientifically Proof</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-
-                        <div class="col-md-2 border">
-                            <div class="specs_box">
-                                <img src="assets/img/smile.svg" alt="" class="img-fluid">
-                                <h4>3,000+ Happy Smiles</h4>
-                            </div>
-                        </div>
-
-                        <div class="col-md-2 ">
-                            <div class="specs_box">
-                                <img src="assets/img/100per.png" alt="" class="img-fluid">
-                                <h4>Satisfaction Guarantee</h4>
-                            </div>
-                        </div>
-
-                        <div class="col-md-2 border">
-                            <div class="specs_box">
-                                <img src="assets/img/relief.png" alt="" class="img-fluid">
-                                <h4>Insant Results</h4>
-                            </div>
-                        </div>
-
-                        <div class="col-md-2">
-                            <div class="specs_box">
-                                <img src="assets/img/scinetiestpro.svg" alt="" class="img-fluid">
-                                <h4>Scientifically Proof</h4>
-                            </div>
-                        </div>
-
                     </div>
                 </div>
             </div>
@@ -97,18 +92,67 @@
         <!-- specification section end -->
 
 
-        <!-- about us section start -->
+        <!-- about us and also service section inside this section start -->
         <section style="background: url(./assets/img/about-bg.png) no-repeat center center / cover;">
             <div class="container py-lg-5 py-4">
                 <div class="row">
-                    <div class="col-md-7"></div>
-                    <div class="col-md-5">
-                        <img src="assets/img/" alt="" class="img-fluid">
+                    <div class="col-md-8">
+                        <div class="about_content">
+                            <div class="about_heading">
+                                <h2>Best, highly qualified clinicians who understand dentistry with holistic approach
+                                    for utmost care of each & every patient</h2>
+                            </div>
+                            <div class="about_para">
+                                <p>Braces Faces superspeciality dental care is founded by Dr.shubhangi
+                                    (MDS-Orthodontist) & Dr Satyajit ( MDS-Oral & Maxillofacial surgeon) with aims to
+                                    provide unparalleled dental, orthodontic, implantology, customise implantology, oral
+                                    cancer surgeries & maxillofacial surgery services at affordable prices. Dr Shubhangi
+                                    is an Orthodontist & licences HAAD practiser in Abu Dhabi who graduated in dentistry
+                                    from Shri Sai Dental college Vikarabad in 2010 and Also completed his post graduate
+                                    fellowship in Orthodontics in 2015. She has learned the best strategic orthodontics
+                                    & Invisalign. She has the experience of more than.</p>
+                            </div>
+                            <div class="know_more">
+                                <a href="" class="btn">Know More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="assets/img/about_us.png" alt="" class="img-fluid">
                     </div>
                 </div>
             </div>
+
+            <!-- service section start -->
+            <section>
+                <div style="background: linear-gradient(0deg, #39cabb73, #ffffff00);">
+                    <div class="container py-md-5 py-2">
+                        <div class="row">
+                            <div class="col-md-10 mx-auto my-md-5 my-2">
+                                <div class="service_heading">
+                                    <h2>We have specialists for every segment of dentistry</h2>
+                                    <p>It's our responsibility to provide the best treatment without pain and keep your
+                                        <br class="d-none d-md-block d-lg-block">
+                                        teeth healthier & give you the best smile.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="service_bx rounded">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- service section end -->
         </section>
-        <!-- about us section end -->
+        <!-- about us and also service section inside this section section end -->
+
+
 
 
 
